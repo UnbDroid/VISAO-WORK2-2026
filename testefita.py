ff7452fac3d06ea2734db4825abf7e8dae41146d
Letícia <letcia.gchavess@gmail.com>
2026-06-09 15:12:10 -0300

    gi

---
+++ testefita.py
@@ -0,0 +1,13 @@
+import cv2
+import sys
+
+
+imagem=cv2.imread("fotofita.jpeg")
+cv2.imshow("PEWPEWPEW", imagem)
+cv2.waitKey(0)
+cv2.imwrite("saidafotofita.jpeg", imagem)
+(B,G,R)=imagem[0,0]
+print(B,G,R) 
+cv2.destroyAllWindows()
+
+ 
