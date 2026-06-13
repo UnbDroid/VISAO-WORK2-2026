d1a7297ad2a1ffc41b55e806c2a57584b25c61c9
Karol <camposanakarol@gmail.com>
2026-06-13 01:02:57 -0300

    Atualizando função cv2.findContours para versão mais atualizada do opencv

--- deteccaoParedesVirtuais/testefita.py
+++ deteccaoParedesVirtuais/testefita.py
@@ -18,20 +18,19 @@ img = cv2.cvtColor(imgColorida, cv2.COLOR_BGR2GRAY)
 #cv2.imwrite("saidafotofita.jpeg", img)
 suave = cv2.GaussianBlur(img, (7, 7), 0) # aplica blur
 (T, bin) = cv2.threshold(suave, 160, 255, cv2.THRESH_BINARY)
-(T, binI) = cv2.threshold(suave, 160, 255,
-cv2.THRESH_BINARY_INV)
+(T, binI) = cv2.threshold(suave, 160, 255,cv2.THRESH_BINARY_INV)
 
 #Passo 4: Detecção de bordas com Canny
 bordas = cv2.Canny(bin, 70, 150)
 
-(lx, objetos, lx) = cv2.findContours(bordas.copy(),
- cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+objetos, _ = cv2.findContours(bordas.copy(), 
+  cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
-escreve(img, "Imagem em tons de cinza", 0)
-escreve(suave, "Suavizacao com Blur", 0)
-escreve(bin, "Binarizacao com Metodo Otsu", 255)
-escreve(bordas, "Detector de bordas Canny", 255)
+print(img, "Imagem em tons de cinza", 0)
+print(suave, "Suavizacao com Blur", 0)
+print(bin, "Binarizacao com Metodo Otsu", 255)
+print(bordas, "Detector de bordas Canny", 255)
 temp = np.vstack([
 np.hstack([img, suave]),
 np.hstack([bin, bordas])
@@ -42,7 +41,7 @@ cv2.imshow("Quantidade de objetos: "+str(len(objetos)), temp)
 cv2.waitKey(0)
 imgC2 = imgColorida.copy()
 cv2.imshow("Imagem Original", imgColorida)
-48
+
 cv2.drawContours(imgC2, objetos, -1, (255, 0, 0), 2)
 escreve(imgC2, str(len(objetos))+" objetos encontrados!")
 cv2.imshow("Resultado", imgC2)
