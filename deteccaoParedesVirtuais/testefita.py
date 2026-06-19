9b9492643dc644c570ab9d842d22b40f16823aa3
Karol <camposanakarol@gmail.com>
2026-06-19 11:42:12 -0300

    Correção do filtro vermelho (máscaras), colocando o Canny para rodar em cima desse filtro e adicão de variações na proporção em caso dos vértices trocarem de ordem

--- deteccaoParedesVirtuais/testefita.py
+++ deteccaoParedesVirtuais/testefita.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 
 #Função para facilitar a escrita nas imagem
-def escreve(img, texto, cor=(255,0,0)):
+def escreve(img, texto, cor=(255,255,255)):
     fonte = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(img, texto, (10,20), fonte, 0.5, cor, 0, cv2.LINE_AA)
 
@@ -15,146 +15,103 @@ if not imgColorida.isOpened():
     print('Não foi possível carregar o vídeo')
     sys.exit()
 
-ok, frame = imgColorida.read()
-if not ok:
-    print("Não foi possível ler o aquivo de vídeo")
-    sys.exit()
-
-ultimo_temp = None 
+ultimo_temp = None
+ultimo_frame = None
 ultimo_objetos = []
 
 while True:
     ok, frame = imgColorida.read()
     if not ok or frame is None:
+        print("Não foi possível ler o aquivo de vídeo")
         break
-
-    #imgColorida = cv2.imread('fotofita.jpeg') #Carregamento da imagem
-    #Se necessário o redimensioamento da imagem pode vir aqui.
-    #Passo 1: Conversão para tons de cinza
+        
+    imgResultado = frame.copy() # cópia p/ desenhar os resultados finais
+    
+    '''#Passo 1: Conversão para tons de cinza
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    suave = cv2.GaussianBlur(img, (7, 7), 0) # aplica blur
+    '''
+    # passo 2: blur para reduzir ruidos
+    suave = cv2.GaussianBlur(frame, (7, 7), 0) # aplica blur
     (T, bin) = cv2.threshold(suave, 160, 255, cv2.THRESH_BINARY)
 
-    #Passo 4: Detecção de bordas com Canny
-    bordas = cv2.Canny(bin, 70, 150)
+    # passo 3: conversão pra hsv
+    hsv = cv2.cvtColor(suave, cv2.COLOR_BGR2HSV)
+
+    red_lower = np.array([0,120,70])
+    red_upper = np.array([10,255,255])
+    red_mask1 = cv2.inRange(hsv, red_lower, red_upper)
+
+    red_lower2 = np.array([170,120,70])
+    red_upper2 = np.array([180,255,255])
+    red_mask2 = cv2.inRange(hsv, red_lower2, red_upper2)
+
+    # máscara final (só com vermelho)
+    red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+
+    #Passo 4: Detecção de bordas com Canny em cima da máscara do vermelho
+    bordas = cv2.Canny(red_mask, 70, 150)
 
     objetos, _ = cv2.findContours(bordas.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    imgResultado = frame.copy() # cópia p/ desenhar os resultados finais
-
     centros = []
-    listras_detectadas = 0 
 
-    #definindo parametros de uma fita para que ela seja detectada
-    
-    continuidade_p = False
-     # quadrilatero_p: se o contorno tem 4 vertices, cor_p: se a cor no centro é vermelha, continuidade_p: se a proporção entre os lados é próxima da proporção_predefinida (ex: 2.0 para uma fita de 20cm x 10cm)
+    listras_detectadas = 0 
 
     #lista contendo os vertices das possiveis listras detectadas:
     vertices_listras = []
-    #print(f"Contornos totais encontrados pelo Canny: {len(objetos)}")
-
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    red_lower = np.array([0,120,70])
-    red_upper = np.array([10,255,255])
-    red_mask = cv2.inRange(hsv, red_lower, red_upper)
+    print(f"Contornos totais encontrados pelo Canny: {len(objetos)}")
 
     for contorno in objetos:
-        quadrilatero_p = False
-        cor_p = False
-        proporcao_p = None
-        # calcula o perimetro do contorno
-        perimetro = cv2.arcLength(contorno, True) # True significa que o contorno é fechado
+        # > 100 é um filtro de área mínima p/ ignorar pequenos ruidos da imagem
+        if cv2.contourArea(contorno) < 100:
+            continue
 
+        perimetro = cv2.arcLength(contorno, True) # True significa que o contorno é fechado
         # 0.04 (4% do perímetro) é uma tolerância comum para detectar retângulos
         aproximacao = cv2.approxPolyDP(contorno, 0.04 * perimetro, True)
-        
-        # encontrar o centro usando momentos da imagem 
-        M = cv2.moments(contorno)
-        if M["m00"] != 0:
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-
-            '''
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            red_lower = np.array([0,120,70])
-            red_upper = np.array([10,255,255])
-            green_lower = np.array([40,50,50])
-            green_upper = np.array([80,255,255])
-            blue_lower = np.array([100,150,0])
-            blue_upper = np.array([140,255,255])
-
-            red_mask = cv2.inRange(hsv, red_lower, red_upper)
-            green_mask = cv2.inRange(hsv, green_lower, green_upper)
-            blue_mask = cv2.inRange(hsv, blue_lower, blue_upper)
-
-            red_pixels = red_mask.sum()
-            green_pixels = green_mask.sum()
-            blue_pixels = blue_mask.sum()
-
-            cor_hsv = frame[cx, cy]
-            
-            print(f"Cor original no centro (HSV): {cor_hsv}")
-            if red_pixels > green_pixels and red_pixels > blue_pixels:
-                cor_p = True
-            '''
-
-            '''print(f"Cor original no centro (BGR): {cor_bgr}")
-            if cor_bgr[2] > 150 and cor_bgr[0] < 100:
-                cor_p = True #Só fazendo os limites de vermelho'''
-
-
-            if red_mask[cy, cx] > 0:
-                cor_p = True
          
         # se a aprox. tem 4 vertices, significa que temos um quadrilátero (uma lista da fita!!)
-        # > 100 é um filtro de área mínima p/ ignorar pequenos ruidos da imagem
-        if len(aproximacao) == 4 and cv2.contourArea(contorno) > 100 and cor_p:
-            quadrilatero_p = True
-            print(f"quadrilatero_p = {quadrilatero_p}")
+        if len(aproximacao) == 4:
+            quadrilatero = True
+            print(f"quadrilatero = {quadrilatero}")
 
-            #Pegando os tres primeiros vertices para calcular os angulos
+            M = cv2.moments(contorno)
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+
+            #Pegando os tres primeiros vertices para conferir a proporção
             v1 = aproximacao[0][0]
             v2 = aproximacao[1][0]
             v3 = aproximacao[2][0]
             
-            d1=math.sqrt((v1[0] - v2[0])**2 + (v2[1] - v1[1])**2)
-            d2=math.sqrt((v2[0] - v3[0])**2 + (v3[1] - v2[1])**2) 
-             
-            proporcao_p=d1/d2
-            print(f"Proporção entre os lados: {proporcao_p:.2f}")
-            #proporção_predefinida da fita então precisa testar primeiro
+            d1 = math.sqrt((v1[0] - v2[0])**2 + (v2[1] - v1[1])**2)
+            d2 = math.sqrt((v2[0] - v3[0])**2 + (v3[1] - v2[1])**2) 
 
-            '''if proporção=proporção_predefinida:
-                proporção_p = True
-            listras_detectadas += 1 #colocar como condicao para analisar os centros depois'''
-
-            cv2.drawContours(imgResultado, [aproximacao], -1, (0, 255, 0), 2)
-        
-        else:
-            print(f"cor_p = {cor_p}")
-            print(f"aproximação = {aproximacao}!!!")
-
-        if quadrilatero_p and cor_p and proporcao_p:
-            centros.append((cx, cy))
-            cv2.circle(imgResultado, (cx, cy), 5, (255, 0, 0), -1) 
+            if d2 != 0:
+                proporcao = d1/d2
+                # como pode inverter largura/altura, dependendo do angulo da câmera, aceitamos a proporção ou o inversi dela
+                # ex: se a fita ideal é 2.0, aceitamos peeto de 2.0 ou perto de 0.5
+                if (1.2 <= proporcao <= 2.8) or (0.35 <= proporcao <= 0,8):
+                    centros.append((cx, cy))
+                    cv2.drawContours(imgResultado, [aproximacao], -1, (0, 255, 0), 2)
+                    cv2.circle(imgResultado, (cx, cy), 5, (255, 0, 0), -1)
+                    
+                    print(f"Proporção entre os lados: {proporcao:.2f}")
+                    #proporção_predefinida da fita então precisa testar primeiro
 
     # ordenando os centros da esquerda p/ direita p/ garantir que vai medir a distancia entre listras consecutivas
     centros = sorted(centros, key = lambda ponto: ponto[0])
-    
-    lista_distancias=[]
 
-    for i in range(len(centros) -1):
-        '''if proporcao_p == None or cor_p == False or quadrilatero_p == False:
-            print(f"-> A listra {i + 1} não atende aos critérios de detecção (quadrilátero: {quadrilatero_p}, cor: {cor_p}, proporção: {proporcao_p})")
-            continue'''
+    lista_distancias = []
+
+    for i in range(len(centros) - 1):
         p1 = centros[i]
         p2 = centros[ i + 1]
 
         # distância euclidiana: d = raiz((x2 - x1)^2 + (y2 - y1)^2)
         distancia = math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+
         lista_distancias.append(distancia)
 
         cv2.line(imgResultado, p1, p2, (0, 255, 255), 2)
@@ -167,17 +124,23 @@ while True:
         
         print(f"Distância entre a listra {i + 1} e a listra {i + 2}: {distancia:.2f} pixels")
 
-    #for i in lista_distancias:
     cv2.imshow('Paredes Virtuais Detectadas', imgResultado)
+
+    # Preparando o Grid de etapas convertendo tudo para 3 canais (BGR)
+    img_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img_cinza_3c = cv2.cvtColor(img_cinza, cv2.COLOR_GRAY2BGR)
+    suave_3c = cv2.cvtColor(cv2.cvtColor(suave, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
+    mask_3c = cv2.cvtColor(red_mask, cv2.COLOR_GRAY2BGR)
+    bordas_3c = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
     
-    escreve(img, "Imagem em tons de cinza", 0)
-    escreve(suave, "Suavizacao com Blur", 0)
-    escreve(bin, "Binarizacao com Metodo Otsu", 255)
-    escreve(bordas, "Detector de bordas Canny", 255)
+    escreve(img_cinza_3c, "Imagem em tons de cinza", 0)
+    escreve(suave_3c, "Suavizacao com Blur", 0)
+    escreve(mask_3c, "Filtro de cor vermelha (HSV)", 255)
+    escreve(bordas_3c, "Detector de bordas Canny", 255)
 
     ultimo_temp = np.vstack([
-    np.hstack([img, suave]),
-    np.hstack([bin, bordas])
+    np.hstack([img_cinza_3c, suave_3c]),
+    np.hstack([mask_3c, bordas_3c])
     ])
     ultimo_frame = frame.copy()
     ultimo_objetos = objetos
